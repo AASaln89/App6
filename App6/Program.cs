97e8bdb0d6a44b1743a533257fb0b250abe32c06
Andrey Salnikov <kvanta13@yandex.ru>
2022-02-19 14:48:09 +0300

    Добавьте файлы проекта.

---
+++ App6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение:");
+            string s = Console.ReadLine();
+            string[] str = s.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int maxlen = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > maxlen)
+                {
+                    maxlen = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово состоит из " + maxlen + " " + "букв");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ App62/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_6._2
+{
+    class Pal
+    {
+        public static bool Palindrom(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+                if (s[i] != s[j])
+                    return false;
+            }
+            return true;
+        }
+        static void Main()
+        {
+            string s;
+            Console.WriteLine("Тест \n\nВведите строку:");
+            s = Console.ReadLine();
+            s = s.Replace(" ", "");
+            if (Palindrom(s) == true)
+                Console.WriteLine("строка - палиндром");
+            else Console.WriteLine("строка - не палиндром");
+            Console.ReadLine();
+        }
+    }
+}
